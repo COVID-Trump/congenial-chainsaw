fc02038982c679b9ae3c353e5a4625f5554293a4
Teddy Li <teddyxlandlee@hotmail.com>
2026-08-13 12:10:41 +0800

    Update 01_tojson.py: fix 12-based hours with adjust_sleep_hour()

--- 01_tojson.py
+++ 01_tojson.py
@@ -11,6 +11,13 @@ MUL_CHOICE_SPLITERATOR = '\u250b'
 # fill_float: list[float]
 
 
+
+def adjust_sleep_hour(x: int) -> int:
+    if x == 12: return 0
+    elif 8 <= x <= 11: return x + 12
+    else: return x
+
+
 def to_json(filename: str | typing.TextIO) -> list[dict]:
     if isinstance(filename, str):
         with open(filename, encoding='utf8') as f:
@@ -18,10 +25,12 @@ def to_json(filename: str | typing.TextIO) -> list[dict]:
     else:
         data = list(csv.reader(filename))
 
-    data = [
+    answers_is_target = [x[2].strip() for x in data[1:]]
+
+    questions, *data = (
         [s.replace('&gt;', '>') for s in x]
         for x in data if x[2].strip() != '否'
-    ]
+    )
     flipped = tuple(zip(*data))[2:] # 0: ORDINAL; 1: DATE; 2: IS_TARGET
 
     if len(flipped) != 63:
@@ -29,7 +38,7 @@ def to_json(filename: str | typing.TextIO) -> list[dict]:
 
     # index sets
     index_fill_ints = {
-        42, # BED_H [8-11 -> 20-23]
+        42, # BED_H [8-11 -> 20-23; 12 -> 0]
         43, # BED_M
         44, # SLEEP_MINS
         45, # WAKE_H
@@ -40,6 +49,9 @@ def to_json(filename: str | typing.TextIO) -> list[dict]:
         47, # ASLEEP_HRS
     }
     index_mul_choice = {61}
+
+    index_is_target = {0}       # choice
+    index_adjust_hours = {42}   # fill_int
 
     res = []
     for index, question in enumerate(flipped):
@@ -59,12 +71,17 @@ def to_json(filename: str | typing.TextIO) -> list[dict]:
         elif index in index_fill_ints:
             _type = 'fill_int'
             answers = list(map(int, answered))
+            if index in index_adjust_hours:
+                answers = list(map(adjust_sleep_hour, answers))
         elif index in index_fill_floats:
             _type = 'fill_float'
             answers = list(map(float, answered))
         else:
             _type = 'choice'
-            answers = list(answered)
+            if index not in index_is_target:
+                answers = list(answered)
+            else:
+                answers = answers_is_target[:]
             has_choices = True
             choices.update(answers)
         obj = {
